eb2973c12719f43265a31e713efef9edea9954ec
Olena Syrotkina <os@Olena-Syrotkina.local>
2025-11-18 12:25:29 -0500

    Prepare Dockerized Django app for Render deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret")
 #DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
